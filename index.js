d54c4806b5f0929a256d3885f737bf7d830d1aa7
Ammar Jamil <ammarjamil7000@gmail.com>
2025-08-28 21:36:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const sh = require("./super-hero.js")
+console.log(sh.getName());
+sh.setName("Superman");
+console.log(sh.getName());
+
+const newsh = require("./super-hero.js");
+console.log(newsh.getName());
