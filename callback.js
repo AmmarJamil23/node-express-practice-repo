6b6dc65e916d2fc351cab5d998df495d363e7af9
Ammar Jamil <ammarjamil7000@gmail.com>
2025-08-30 23:16:52 +0500

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,14 @@
+function getData(dataID, getNextData) {
+    setTimeout( () => {
+        console.log("data", dataID);
+        if(getNextData){
+             getNextData();
+        }
+    }, 2000)
+}
+
+getData(1, () => {
+    getData(2, () => {
+        getData(3);
+    });
+});
