cb86ecbc6325bcaa4dc57f0e78cd446d3d2dc998
Ammar Jamil <ammarjamil7000@gmail.com>
2025-09-02 22:07:14 +0500

    Add files via upload

---
+++ pattern.js
@@ -0,0 +1,10 @@
+const math = require("./math")
+
+const {add, sub} = math
+
+// console.log(math.add(4, 67));
+// console.log(math.sub(4, 67));
+
+console.log(add(2,3));
+
+console.log(sub(65,12));
