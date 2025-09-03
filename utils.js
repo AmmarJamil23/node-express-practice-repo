0738a8db94be467ab3df40f49b278429276dcf97
Ammar Jamil <ammarjamil7000@gmail.com>
2025-09-03 16:03:59 +0500

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,14 @@
+function generateRnadomNumber () {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function celciusToFahrenheit (celcius) {
+    return (celcius * 9) / 5 + 32;
+
+}
+
+module.exports = {
+    generateRnadomNumber,
+    celciusToFahrenheit,
+
+};
