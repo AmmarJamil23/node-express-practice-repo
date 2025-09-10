d0931474ad69bd7985b54c3f1d58ab162cc9f301
Ammar Jamil <ammarjamil7000@gmail.com>
2025-09-10 17:21:51 +0500

    Add files via upload

---
+++ importjson.js
@@ -0,0 +1,9 @@
+/*
+JSON is a data interchange format commonly used by
+web servers.
+Node gives .js files priority over .json filles
+*/
+
+// const data = require("./data");
+// console.log(data.name);
+console.log("Hello ABC");
